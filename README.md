bd00a329a284c98cae7089217c6b389dab365dfb
Krivich Ekaterina <krivich.ekaterina@gmail.com>
2014-06-04 15:34:41 +0400

    changes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### postreSQL readme
 https://wiki.postgresql.org/wiki/PostgreSQL_For_Development_With_Vagrant
+
+### mysql
+is not automated yet - read https://gist.github.com/johnantoni/3288329 to install
--- Vagrant-setup/install-system.sh
+++ Vagrant-setup/install-system.sh
@@ -1,2 +1,9 @@
 #!/bin/sh -e
 sudo apt-get install curl -y
+sudo apt-get install libicu-dev -y
+
+#postgres
+sudo apt-get install libpq-dev -y
+
+# mysql
+sudo apt-get install libmysql-ruby libmysqlclient-dev -y
